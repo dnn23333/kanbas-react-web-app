e973ea3052719a7287bc3fd294fc18cc824f1188
董寧寧 <dongningning@dongningningdeMacBook-Air.local>
2023-12-12 17:44:39 -0800

    JavaScript and React.js Assignment 3

---
+++ src/Kanbas/Courses/Assignments/AssignmentEditor/AssignmentEditBar.js
@@ -0,0 +1,21 @@
+import { AiOutlineCheckCircle } from "react-icons/ai";
+import "./index.css";
+
+function AssignmentEditBar() {
+  return (
+      <>
+        <input
+            className="form-control w-25"
+            style={{ display: "inline" }}
+            type="text"
+            placeholder="Search for Assignments"
+        />
+        <div className="d-flex flex-nowrap float-end align-items-center">
+          <AiOutlineCheckCircle className="published" />
+          <span className="fw-semibold me-4 published">Published</span>
+        </div>
+      </>
+  );
+}
+
+export default AssignmentEditBar;
---
+++ src/Labs/index.js
@@ -0,0 +1,15 @@
+import Assignment3 from "./a3";
+import Nav from "../Nav";
+import PathParameters from "./a3/PathParameters"
+import Classes from "./a3/Classes";
+function Labs() {
+  return(
+      <div>
+       <Nav/>
+        <Assignment3/>
+        <PathParameters/>
+      </div>
+  );
+}
+export default Labs;
+
