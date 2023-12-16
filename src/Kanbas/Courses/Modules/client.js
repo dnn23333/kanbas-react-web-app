824f207e1c9c235ef0624bb276540f54948ddace
董寧寧 <dongningning@dongningningdeMacBook-Air.local>
2023-12-16 11:33:30 -0800

    a5

--- src/Kanbas/Courses/Modules/client.js
+++ src/Kanbas/Courses/Modules/client.js
@@ -1,8 +1,7 @@
 import axios from "axios";
-/*const COURSES_URL = "https://kanbas-node-server-app-09ey.onrender.com/api/courses";
-const MODULES_URL = "https://kanbas-node-server-app-09ey.onrender.com/api/modules";*/
-const API_BASE = process.env.REACT_APP_API_BASE;
-const MODULES_URL = `${API_BASE}/modules`;
+const COURSES_URL = "https://kanbas-node-server-app-09ey.onrender.com/api/courses";
+const MODULES_URL = "https://kanbas-node-server-app-09ey.onrender.com/api/modules";
+
 
 export const updateModule = async (module) => {
   const response = await axios.
